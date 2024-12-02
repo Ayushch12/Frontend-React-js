ab1a710e66ef99a956b2a454372b6fd9182a0b3b
Ayush chalisee <ayushchalise12@gmail.com>
2024-12-02 19:21:28 +0100

    Added the fetch api CRUD

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8000"; // Use HTTP for local development if HTTPS causes issues
+const API_BASE_URL = "http://localhost:8000";
 
 const axiosInstance = axios.create({
     baseURL: API_BASE_URL,
@@ -16,32 +16,32 @@ export const getProducts = async () => {
     return response.data;
 };
 
-// export const getCategories = async () => {
-//     const response = await axiosInstance.get("/categories");
-//     return response.data;
-// };
+export const getCategories = async () => {
+    const response = await axiosInstance.get("/categories");
+    return response.data;
+};
 
-// export const addProduct = async (product) => {
-//     const response = await axiosInstance.post("/products", product);
-//     return response.data;
-// };
+export const addProduct = async (product) => {
+    const response = await axiosInstance.post("/products", product);
+    return response.data;
+};
 
-// export const updateProduct = async (id, product) => {
-//     const response = await axiosInstance.put(`/products/${id}`, product);
-//     return response.data;
-// };
+export const updateProduct = async (id, product) => {
+    const response = await axiosInstance.put(`/products/${id}`, product);
+    return response.data;
+};
 
-// export const deleteProduct = async (id) => {
-//     const response = await axiosInstance.delete(`/products/${id}`);
-//     return response.data;
-// };
+export const deleteProduct = async (id) => {
+    const response = await axiosInstance.delete(`/products/${id}`);
+    return response.data;
+};
 
-// export const addCategory = async (category) => {
-//     const response = await axiosInstance.post("/categories", category);
-//     return response.data;
-// };
+export const addCategory = async (category) => {
+    const response = await axiosInstance.post("/categories", category);
+    return response.data;
+};
 
-// export const deleteCategory = async (id) => {
-//     const response = await axiosInstance.delete(`/categories/${id}`);
-//     return response.data;
-// };
+export const deleteCategory = async (id) => {
+    const response = await axiosInstance.delete(`/categories/${id}`);
+    return response.data;
+};
